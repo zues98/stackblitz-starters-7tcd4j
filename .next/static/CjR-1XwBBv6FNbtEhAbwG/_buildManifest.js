17653a1e27c415759186a8c719685fbb980b9606
zues98 <135609058+zues98@users.noreply.github.com>
2023-06-16 22:00:37 +0000

    war robots

---
+++ .next/static/CjR-1XwBBv6FNbtEhAbwG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/92ff1f22dd9892a3.css","static/chunks/pages/index-2c1bc3f7daebed2e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
